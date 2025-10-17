924861fce444cdb75e1706e8cdae9a0c1b09fba5
Frigzer <borekadam89@gmail.com>
2025-10-17 10:49:11 +0200

    Update FollowCamera.cs

--- My project/Assets/FollowCamera.cs
+++ My project/Assets/FollowCamera.cs
@@ -5,11 +5,24 @@ using UnityEngine;
 public class FollowCamera : MonoBehaviour
 {
 
-    public Transform lookAtTarget;
+    public Transform target;
+    public Vector3 offsetPosition;
+    public float smoothSpeed = 0.05f;
+    public Vector3 lookAtOffset;
 
     // Update is called once per frame
-    void FixedUpdate()
+    private void FixedUpdate()
     {
-        transform.LookAt(lookAtTarget.position);
+        if(target==null) {
+            Debug.LogWarning("Missing target ref !", this);
+            return;
+        }
+
+        Vector3 desiredPosition = target.TransformPoint(offsetPosition);
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+
+        transform.position = smoothedPosition;
+        transform.LookAt(target.position+lookAtOffset);
+
     }
 }
